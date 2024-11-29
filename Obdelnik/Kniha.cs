6a082af464f4ceea5c8b644d747b83dc8f89ecc9
krystofbures <98737@student.vitkovickastredni.cz>
2024-11-29 15:33:45 +0100

    Update Kniha.cs

--- Obdelnik/Kniha.cs
+++ Obdelnik/Kniha.cs
@@ -2,18 +2,34 @@ namespace Dedeicnost
 {
     public abstract class Kniha
     {
-        protected String Autor;
-        protected String Nazev;
-        protected int RokVydani;
-        public abstract String VypisInformace();
+        public string Nazev;
+        public string Autor;
+        public int RokVydani; 
+        public static string VypsiInformaci();
+
+        //Konstruktor
+        public Kniha(string nazev, string autor, int rokVydani)
+        {
+            Nazev = nazev;
+            Autor = autor;
+            RokVydani = rokVydani;
+        }
+
+        public virtual string VypisInformaci()
+        {
+            return Console.WriteLine(nazev", " + autor", " + rokVydani);
+        }
     } 
 
      public class Ebook : Kniha
     {
-        public (double VelikostSouboru)
+        public double VelikostSouboru;
+        public string Format;
+
+        public override string VypisInformaci()
         {
-            this. = ;
-            this. = ;            
+            return Console.WriteLine(nazev", " + autor", " + rokVydani", " + VelikostSouboru", " + Format);
         }
+
     }
 }
