404515b41a28577a25ddcebb556561eba30a7b84
krystofbures <98737@student.vitkovickastredni.cz>
2024-12-06 09:39:29 +0100

    CV - Knihovna

--- Obdelnik/Kniha.cs
+++ Obdelnik/Kniha.cs
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 namespace ConsoleApp
 {
     public class Kniha
-=======
-namespace Dedicnost
-{
-    public class Kniha 
->>>>>>> 1a726c145ff16eb9719cf7331b6e7869f0d7f5a2
     {
+        //Vytvoření vylstnosti třidy kniha
         public string Nazev;
         public string Autor;
         public int RokVydani;
-<<<<<<< HEAD
 
+        //Knostruktor pro přeměnu vlastnsoti na paramtry
         public Kniha(string nazev, string autor, int rokVydani) 
         {
             this.Nazev = nazev;
@@ -22,7 +17,7 @@ namespace Dedicnost
             Knihovna.PridatKnihu(this);
         }
 
-
+        //Vypis informací   VIRTUAL - metoda co muže byt přepsana a upravena
         public virtual string VypisInformace()
         {
             return "Název: " + Nazev + ", Autor: " + Autor + ", Rok vydání: " + RokVydani;
@@ -30,48 +25,22 @@ namespace Dedicnost
 
     }
 
+    //Třída Ebook která dědí od třídy Kniha
     public class Ebook : Kniha 
     {
         public double VelikostSouboru;
         public string Format;
 
+        //Knostruktor pro přeměnu vlastnsoti na paramtry BASE - umožňuje děděné třídě přístup k členům vyšší třídy (nemusíse použít pokud zakladní třída nemá parametry)
         public Ebook(string nazev, string autor, int rokVydani, string Format, double VelikostSouboru) : base(nazev, autor, rokVydani)
         {
             this.VelikostSouboru = VelikostSouboru;
             this.Format = Format;
-=======
-
-        public Kniha(string nazev, string autor, int rokVydani) 
-        {
-            this.Nazev = nazev;
-            this.Autor = autor;
-            this.RokVydani = rokVydani;
         }
 
-
-        public virtual string VypisInformace()
-        {
-            return "Název:" + Nazev + ", Autor: " + Autor + ", Rok vydání: " + RokVydani;
-        }
-
-    } 
-
-      public class Ebook : Kniha 
-    {
-        public double VelikostSouboru;
-        public string Format;
-
-        //Konstruktor pro Ebook base = přistup k členum konstruktorů
-        public Ebook(string nazev, string autor, int rokVydani, string Format, double VelikostSouboru) : base(nazev, autor, rokVydani)
-        {
-            this.VelikostSouboru = velikostSouboru;
-            this.Format = format;
->>>>>>> 1a726c145ff16eb9719cf7331b6e7869f0d7f5a2
-        }
-
+        //Vypis informací
         public override string VypisInformace()
         {
-<<<<<<< HEAD
             return base.VypisInformace() + ", Velikost Souboru: " + VelikostSouboru + ", formát: " + Format;
         }
     }
@@ -80,29 +49,13 @@ namespace Dedicnost
     {
         public static int PocetKnih;
 
+        //Tento kód vytváří statický seznam knih který je přístupný z celé třídy
         public static List<Kniha> knihovna = new List<Kniha>();
         
+        //Tento kód definuje statickou metodu PridatKnihu, která přidá Knihu
         public static void PridatKnihu(Kniha kniha)
         {
-            knihovna.Add(kniha);
-=======
-            return base.VypisInformace() + ", Velikost Souboru: " + velikostSouboru + ", formát: " + format;
-        }
-
-        public static class Knihovna
-    {
-        public static int PocetKnih;
-
-        //vytváří statickou proměnnou knihovna, která je seznamem objektů typu 
-        public static List<Kniha> knihovna = new List<Kniha>();
-        
-        // přidává objekt typu Kniha do statického seznamu knihovna a zvyšuje čítač knih
-        public static void PridatKnihu(Kniha kniha)
-        {
-            //Přidá objekt kniha do seznamu knihovna
-            knihovna.Add(kniha);
-            // zvyšuje proměnnou PocetKnih o 1 
->>>>>>> 1a726c145ff16eb9719cf7331b6e7869f0d7f5a2
+            //Zvýší počet knih o 1
             PocetKnih++;
         }
     }
